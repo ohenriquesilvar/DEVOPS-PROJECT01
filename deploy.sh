85d1e2858ff6a8f3f417063ba7f80adf5216122a
ohenriquesilvar <ohenriquerabelo@gmail.com>
2024-08-21 13:51:43 -0300

    feat: :sparkles: update deploy

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,31 @@
-
 #!/bin/sh
 
+# Função para verificar se todos os pods estão prontos
+check_pods_ready() {
+  echo "Verificando se todos os pods estão prontos..."
+  local namespace=$1
+  local timeout=$2
+  local start_time=$(date +%s)
+
+  while true; do
+    local elapsed_time=$(($(date +%s) - start_time))
+    if [ "$elapsed_time" -ge "$timeout" ]; then
+      echo "Tempo limite de verificação excedido."
+      return 1
+    fi
+
+    # Verifica o status dos pods
+    if kubectl get pods --namespace "$namespace" | grep -E '0/1'; then
+      echo "Alguns pods ainda não estão prontos. Verificando novamente em 10 segundos..."
+      sleep 10
+    else
+      echo "Todos os pods estão prontos."
+      return 0
+    fi
+  done
+}
+
+# Remove releases existentes
 helm delete frontend
 helm delete backend
 helm delete postgres
@@ -19,13 +44,21 @@ cd ..
 cd frontend-helm || { echo "Failed to cd to frontend-helm"; exit 1; }
 helm install frontend . -f values.yaml
 
-# Aguarda 5 segundos
-echo "Aguardando 10 segundos para garantir que os serviços estejam prontos..."
-sleep 10
+# Aguarda até que todos os pods estejam prontos
+echo "Aguardando até que todos os pods estejam prontos..."
+if check_pods_ready "default" 300; then
+  echo "Todos os pods estão prontos. Iniciando o port-forwarding."
 
-echo "Port-forwarding do frontend iniciado. Acesse http://localhost:3000 para visualizar o frontend."
+  
+  echo "Acesse o frontend em http://localhost:3000"
 
-# Inicia o port-forwarding
-kubectl port-forward --namespace default svc/frontend-frontend-helm 3000:3000
+  
+  # Inicia o port-forwarding
+  kubectl port-forward --namespace default svc/frontend-frontend-helm 3000:3000
 
 
+
+else
+  echo "Não foi possível garantir que todos os pods estão prontos. Abortando o port-forwarding."
+  exit 1
+fi
